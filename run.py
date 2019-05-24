7b77b93e54c423571e72928032e11d5538b70d00
fengjc <jcfeng2013@gmail.com>
2019-05-25 03:10:17 +0800

    test video download ok

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import requests
 
-url = 'https://cn-jxjj-dx-v-03.acgvideo.com/upgcxcode/16/56/93555616/93555616-1-30280.m4s?expires=1558727700&platform=pc&ssig=A8V3jrho790D9U0IDOGEBw&oi=2004568188&trid=2f2f9aa00e1e4249847435155ff28201&nfb=maPYqpoel5MI3qOUX6YpRA==&nfc=1'
+sound_url = 'https://cn-jxjj-dx-v-03.acgvideo.com/upgcxcode/16/56/93555616/93555616-1-30280.m4s?expires=1558730700&platform=pc&ssig=49SEiXa3jXJ-mncm-SH7Cw&oi=2004568188&trid=8dd55c83f4cc4b7bbead427bc698211b&nfb=maPYqpoel5MI3qOUX6YpRA==&nfc=1'
+video_url = 'https://cn-jxjj-dx-v-03.acgvideo.com/upgcxcode/16/56/93555616/93555616-1-30080.m4s?expires=1558730400&platform=pc&ssig=ShAuuRKyqj92PDlvtuBLUg&oi=2004568188&trid=28d5948c56624a6aa316daf11f04f33f&nfb=maPYqpoel5MI3qOUX6YpRA==&nfc=1'
+dm_url = 'https://api.bilibili.com/x/v1/dm/list.so?oid=93555616'
 img_url = 'https://errorpage.b0.upaiyun.com/sfault-image-404'
 
 headers = {
@@ -12,9 +14,9 @@ headers = {
     'Accept': '*/*',
     'Referer': 'https://www.bilibili.com/video/av53476879',
     'Accept-Encoding': 'identity',
-    'Rangee': 'bytes=3095606-3183466',
+    'Range': 'bytes=0-',
     'Accept-Language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,la;q=0.7',
-    'If-Range': '5ce80e54-8fc6d3'
+    'If-Range': '0-'
 }
 
 
@@ -24,7 +26,7 @@ def base_download(url, filename, headers=None):
     with session.get(url, headers=headers) as r:
         r.raise_for_status()
         with open(filename, 'wb+') as f:
-            for chunk in r.iter_content():
+            for chunk in r.iter_content(chunk_size=1024):
                 if chunk:
                     f.write(chunk)
                     if not flag:
@@ -33,8 +35,14 @@ def base_download(url, filename, headers=None):
 
 
 def download_video():
-    res = base_download(url=url, filename='./temp.mp4', headers=headers)
+    res = base_download(url=video_url, filename='./temp_video.mp4', headers=headers)
+    res = base_download(url=sound_url, filename='./temp_sound.mp4', headers=headers)
     print(f'video download {res}')
+
+
+
+def download_dm():
+    pass
 
 
 def download_img():
@@ -42,7 +50,6 @@ def download_img():
     print(f'img download {res}')
 
 
-
 if __name__ == '__main__':
-    # download_video()
-    download_img()
+    download_video()
+    # download_img()
