02e7028367b04a3cd993232a1a8c8c5f25033c9d
fengjc <jcfeng2013@gmail.com>
2019-05-25 20:53:47 +0800

    fix av810872 with get_video_link different response, add secure_string method, add filename judging

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 # python 3.6
 
 
-UP_ID = 404427361
-AV_ID = []
-# AV_ID = [53454693]
+UP_ID = None
+# UP_ID = 404427361
+# AV_ID = []
+AV_ID = [810872]
 
 BASE_DIR = './av'
--- run.py
+++ run.py
@@ -5,11 +5,13 @@ from common.save import SaveTool
 from common.url import headers
 from common.base import create_folder, create_path, check_path
 from api import build_av_info_url, build_submit_videos_url, build_video_info_url
-from config import UP_ID, BASE_DIR
+from config import AV_ID, UP_ID, BASE_DIR
 
 
 def get_up_avs(mid):
     av_list = []
+    if not mid:
+        return av_list
     page = 0
     while True:
         page += 1
@@ -39,37 +41,49 @@ def get_video_link(aid, cid):
     url = build_video_info_url(aid, cid)
     res = HttpTool.get(url, retFormat='json')
     if res and res['code'] == 0:
-        dash = res['data']['dash']
-        video_link = dash['video'][-1]['baseUrl']
-        sound_link = dash['audio'][1]['baseUrl']
+        data = res['data']
+        if 'dash' in data:
+            dash = data['dash']
+            video_link = dash['video'][-1]['baseUrl']
+            sound_link = dash['audio'][1]['baseUrl']
+        elif 'durl' in data:
+            video_link = data['durl'][0]['url']
     return video_link, sound_link
 
 
+def secure_string(string):
+    string = string.replace('！', '!').replace('\\', '_').replace('/', '_')
+    string = string.replace('？', '_').replace('<', '_').replace('>', '_')
+    string = string.replace('<', '_').replace('|', '1').replace('*', '.')
+    string = string.replace(':', '.').replace('"', '\'').strip()
+    return string
+
+
 def main():
-    mid = UP_ID
-    av_list = get_up_avs(mid)
+    av_list = get_up_avs(UP_ID)
+    av_list.extend(AV_ID)
     for av in av_list:
-        author = av['author']
-        aid = av['aid']
+        aid = av['aid'] if isinstance(av, dict) else av
         video_data = get_video_by_aid(aid)
-        title = video_data['title']
+        title = secure_string(video_data['title'])
+        owner = video_data['owner']
         for video in video_data['pages']:
             cid = video['cid']
             part = video['part']
             video_link, sound_link = get_video_link(aid, cid)
-            path = create_folder(BASE_DIR, f'{mid}_{author}', f'{aid}_{title}')
+            path = create_folder(BASE_DIR, f'{owner["mid"]}_{secure_string(owner["name"])}', f'{aid}_{title}')
             video_name, sound_name = create_path(path, f'{cid}_{title}_{part}.mp4'), create_path(path, f'{cid}_{title}_{part}.m4a')
-            if check_path(video_name):
-                continue
-            download_video(video_link, video_name, sound_link, sound_name)
+            if not video_link or check_path(video_name):
+                print(f'{video_name} already exists or link is null')
+            else:
+                download_video(video_link, video_name, headers=headers)
+            if not sound_link or check_path(sound_name):
+                print(f'{sound_name} already exists or link is null')
+            else:
+                download_video(sound_link, sound_name, headers=headers)
 
 
-def download_video(video_link, video_name, sound_link, sound_name):
-    base_download(video_link, video_name, headers=headers)
-    base_download(sound_link, sound_name, headers=headers)
-
-
-def base_download(url, filename, headers=None):
+def download_video(url, filename, headers=None):
     r = HttpTool.get(url, headers=headers, retFormat='raw')
     if not r:
         print(f'{filename} download fail')
